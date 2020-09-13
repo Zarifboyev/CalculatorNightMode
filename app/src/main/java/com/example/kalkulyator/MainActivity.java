5e94075cd6a36c8c58d3bfe97fa42d3b946187f5
Zarifboyev <zarifboyevjavohir27042006@gmail.com>
2020-09-13 13:18:11 +0500

    Calculator Night Mode Error

--- app/src/main/java/com/example/kalkulyator/MainActivity.java
+++ app/src/main/java/com/example/kalkulyator/MainActivity.java
@@ -13,12 +13,15 @@ import org.mozilla.javascript.Scriptable;
 public class MainActivity extends AppCompatActivity {
 
 
-    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, buttonPercent, buttonPlus, buttonMinus, buttonMultiply, buttonDivision, buttonEqual, buttonClear, buttonDot, buttonBracket;
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, buttonQavs2, buttonPlus, buttonMinus, buttonMultiply, buttonDivision, buttonEqual, buttonClear, buttonDot, buttonQavs1;
     TextView textViewInput, textViewOutput;
     String process;
-    boolean checkBracket = false;
+    boolean check = false;
+    boolean lastDot = true;
+    boolean lastNumber = false;
+    boolean tekshiruvchi = false;
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint({"SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,116 +47,104 @@ public class MainActivity extends AppCompatActivity {
 
         buttonClear = findViewById(R.id.buttonClear);
         buttonDot = findViewById(R.id.buttonDot);
-        buttonPercent = findViewById(R.id.buttonPercent);
-        buttonBracket = findViewById(R.id.buttonBracket);
-
+        buttonQavs2 = findViewById(R.id.buttonQavs2);
+        buttonQavs1 = findViewById(R.id.buttonQavs1);
         textViewInput = findViewById(R.id.tvInput);
         textViewOutput = findViewById(R.id.tvOutput);
+
         buttonClear.setOnClickListener(v -> {
+            textViewInput.setTextColor(getResources().getColor(R.color.black));
+            textViewOutput.setTextColor(getResources().getColor(R.color.black));
+            if (textViewInput.getText().equals("")) {
+                textViewInput.setText("");
+            } else {
+                int len = textViewInput.getText().length();
+                String s = textViewInput.getText().toString();
+                if (len > 0) {
+                    textViewInput.setText(textViewInput.getText().subSequence(0, textViewInput.getText().length() - 1));
+                    lastDot = true;
+                    tekshiruvchi = true;
+                }
+
+            }
+        });
+
+        buttonClear.setOnLongClickListener(view -> {
             textViewInput.setText("");
             textViewOutput.setText("");
+            textViewInput.setTextColor(getResources().getColor(R.color.black));
+            textViewOutput.setTextColor(getResources().getColor(R.color.black));
+            lastDot = true;
+            lastNumber = false;
+            check = false;
+            tekshiruvchi = false;
+            return true;
         });
 
-
-        btn0.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "0");
-        });
-
-        btn1.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "1");
-        });
-
-        btn2.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "2");
-        });
-
-        btn3.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "3");
-        });
-
-        btn4.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "4");
-        });
-
-        btn5.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "5");
-        });
-
-        btn6.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "6");
-        });
-
-        btn6.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "6");
-        });
-
-        btn7.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "7");
-        });
-
-        btn8.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "8");
-        });
-
-        btn9.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "9");
-        });
+        for (Button btn : new Button[]{
+                btn0, btn1, btn2, btn3, btn4, btn5, btn6,
+                btn7, btn8, btn9, btn0, buttonQavs1, buttonQavs2
+        }) {
+            btn.setOnClickListener(v -> {
+                lastNumber = true;
+                process = textViewInput.getText().toString();
+                textViewInput.setText(process + ((Button) v).getText().toString());
+                tekshiruvchi = true;
+            });
+        }
 
         buttonPlus.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "+");
+            if (tekshiruvchi) {
+                lastDot = true;
+                lastNumber = false;
+                process = textViewInput.getText().toString();
+                textViewInput.setText(process + "+");
+                tekshiruvchi = false;
+            }
         });
 
 
         buttonMinus.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "-");
+            if (tekshiruvchi) {
+                lastDot = true;
+                lastNumber = false;
+                process = textViewInput.getText().toString();
+                textViewInput.setText(process + "-");
+                tekshiruvchi = false;
+            }
         });
 
         buttonMultiply.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "×");
+            if (tekshiruvchi) {
+                lastDot = true;
+                lastNumber = false;
+                process = textViewInput.getText().toString();
+                textViewInput.setText(process + "×");
+                tekshiruvchi = false;
+            }
         });
 
         buttonDivision.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "÷");
-        });
-
-        buttonDot.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + ".");
-        });
-
-        buttonPercent.setOnClickListener(v -> {
-            process = textViewInput.getText().toString();
-            textViewInput.setText(process + "%");
-        });
-
-        buttonBracket.setOnClickListener(v -> {
-
-            if (checkBracket) {
+            if (tekshiruvchi) {
+                lastDot = true;
+                lastNumber = false;
                 process = textViewInput.getText().toString();
-                textViewInput.setText(process + ")");
-                checkBracket = false;
+                textViewInput.setText(process + "÷");
+                tekshiruvchi = false;
+            }
+        });
+
+        buttonDot.setOnClickListener(view -> {
+            if (lastDot && lastNumber) {
+                process = textViewInput.getText().toString();
+                textViewInput.setText(process + ".");
+                lastDot = false;
             } else {
                 process = textViewInput.getText().toString();
-                textViewInput.setText(process + "(");
-                checkBracket = true;
+                textViewInput.setText(process + "");
             }
-
         });
+
         buttonEqual.setOnClickListener(v -> {
             process = textViewInput.getText().toString();
 
@@ -172,12 +163,11 @@ public class MainActivity extends AppCompatActivity {
                 Scriptable scriptable = rhino.initStandardObjects();
                 finalResult = rhino.evaluateString(scriptable, process, "javascript", 1, null).toString();
             } catch (Exception e) {
-                finalResult = "0";
+                finalResult = "Xato ifoda";
+                textViewInput.setTextColor(getResources().getColor(R.color.red));
+                textViewOutput.setTextColor(getResources().getColor(R.color.red));
             }
-
             textViewOutput.setText(finalResult);
         });
-
-
     }
 }
