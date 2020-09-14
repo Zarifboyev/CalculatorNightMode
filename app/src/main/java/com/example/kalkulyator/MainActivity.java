6131c4972e60af483c48871d4b10b409df646030
Zarifboyev <70443883+Zarifboyev@users.noreply.github.com>
2020-09-14 14:19:44 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/kalkulyator/MainActivity.java
+++ app/src/main/java/com/example/kalkulyator/MainActivity.java
@@ -14,19 +14,20 @@ public class MainActivity extends AppCompatActivity {
 
 
     Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, buttonQavs2, buttonPlus, buttonMinus, buttonMultiply, buttonDivision, buttonEqual, buttonClear, buttonDot, buttonQavs1;
+    //textViewInputda ekranga kiritilayotgan amallar aks etadi. TextVIewOutputda natija aks etadi
     TextView textViewInput, textViewOutput;
     String process;
     boolean check = false;
     boolean lastDot = true;
     boolean lastNumber = false;
     boolean tekshiruvchi = false;
-
-    @SuppressLint({"SetTextI18n"})
+    boolean tozalovchi = false;
+    @SuppressLint({"SetTextI18n", "ResourceType"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+      // Tugmalar Id yordamida topib olinadi
         btn0 = findViewById(R.id.btn0);
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
@@ -51,29 +52,31 @@ public class MainActivity extends AppCompatActivity {
         buttonQavs1 = findViewById(R.id.buttonQavs1);
         textViewInput = findViewById(R.id.tvInput);
         textViewOutput = findViewById(R.id.tvOutput);
-
+     //Agar Delete tugmasi bosilsa o'chiradi
         buttonClear.setOnClickListener(v -> {
-            textViewInput.setTextColor(getResources().getColor(R.color.black));
-            textViewOutput.setTextColor(getResources().getColor(R.color.black));
             if (textViewInput.getText().equals("")) {
                 textViewInput.setText("");
             } else {
+                // len nomli o'zgaruvchida textViewInputni uzunligi saqlanadi
                 int len = textViewInput.getText().length();
+                // s da textViewInput dagi matn saqlanadi
                 String s = textViewInput.getText().toString();
+                // Agar textViewInput ni uzunligi 0 dan katta bo'lsa quyidagi ammalarni bajaradi
                 if (len > 0) {
+                    textViewInput.setTextColor(getResources().getColor(R.color.forTextView));
+                    textViewOutput.setTextColor(getResources().getColor(R.color.forTextView));
                     textViewInput.setText(textViewInput.getText().subSequence(0, textViewInput.getText().length() - 1));
+                    textViewOutput.setText("");
                     lastDot = true;
                     tekshiruvchi = true;
                 }
 
             }
         });
-
         buttonClear.setOnLongClickListener(view -> {
-            textViewInput.setText("");
-            textViewOutput.setText("");
-            textViewInput.setTextColor(getResources().getColor(R.color.black));
-            textViewOutput.setTextColor(getResources().getColor(R.color.black));
+            Clear();
+            textViewInput.setTextColor(getResources().getColor(R.color.forTextView));
+            textViewOutput.setTextColor(getResources().getColor(R.color.forTextView));
             lastDot = true;
             lastNumber = false;
             check = false;
@@ -82,10 +85,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
         for (Button btn : new Button[]{
-                btn0, btn1, btn2, btn3, btn4, btn5, btn6,
+                btn0, btn2, btn3, btn4, btn5, btn6,
                 btn7, btn8, btn9, btn0, buttonQavs1, buttonQavs2
         }) {
             btn.setOnClickListener(v -> {
+                if (tozalovchi){
+                    Clear();
+                }
+                tozalovchi = false;
                 lastNumber = true;
                 process = textViewInput.getText().toString();
                 textViewInput.setText(process + ((Button) v).getText().toString());
@@ -168,6 +175,13 @@ public class MainActivity extends AppCompatActivity {
                 textViewOutput.setTextColor(getResources().getColor(R.color.red));
             }
             textViewOutput.setText(finalResult);
+
+            tozalovchi = true;
         });
+    }
+
+    private void Clear (){
+        textViewInput.setText("");
+        textViewOutput.setText("");
     }
 }
